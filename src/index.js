a2e08905ab4bdb3f7ea0257f539d5858f85a1602
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-02-12 17:39:15 -0500

    Change ToDoItem's constructor
    Default parameters ensure class integrity and make data serializable

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import ToDoItem from "./toDoItem.js";
 import { format } from "date-fns";
 
-let toDoItem_1 = new ToDoItem(
-    "Item 1", 
-    "Be awesome", 
-    Date.now(), 
-    1
-)
+let toDoItem_1 = new ToDoItem();
 
 console.log(toDoItem_1);
-console.log(format(toDoItem_1.dueDate, "yyyy-MM-dd"));
+console.log(format(toDoItem_1.dueDate, "yyyy-MM-dd H:m:s"));
--- src/toDoItem.js
+++ src/toDoItem.js
@@ -1,10 +1,22 @@
 export default class ToDoItem {
-    constructor(title, description, dueDate, priority){
+    constructor(
+        title = crypto.randomUUID(), 
+        description = "None", 
+        dueDate = Date.now(), 
+        priority = 99,
+        notes = "None",
+        checklist = {}
+    )
+        /* REQUIRES: checklist is an object with key value pair:
+            String checklist-item: Boolean completed
+        */
+    
+    {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.notes = "None";
-        this.checklist = {} // String checklist-item: Boolean completed
+        this.notes = notes;
+        this.checklist = checklist;
     }
 }
