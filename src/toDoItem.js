34dcd5694812e334dd4c001efe68bbfd0e4d508f
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-02-13 11:52:59 -0500

    Add ID property to ToDoItem

--- src/toDoItem.js
+++ src/toDoItem.js
@@ -1,11 +1,12 @@
 export default class ToDoItem {
     constructor(
-        title = crypto.randomUUID(), 
+        title = "Untitled", 
         description = "None", 
         dueDate = Date.now(), 
         priority = 99,
         notes = "None",
-        checklist = {}
+        checklist = {},
+        toDoItemID = crypto.randomUUID(),
     )
         /* REQUIRES: checklist is an object with key value pair:
             String checklist-item: Boolean completed
@@ -18,5 +19,6 @@ export default class ToDoItem {
         this.priority = priority;
         this.notes = notes;
         this.checklist = checklist;
+        this.id = toDoItemID;
     }
 }
