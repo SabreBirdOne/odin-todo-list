03a3ce97af570e0ceadad8b7af6f776e8730c126
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-02-13 20:12:25 -0500

    Add project card factory

--- src/cardFactories.js
+++ src/cardFactories.js
@@ -11,12 +11,35 @@ const createToDoItemCard = function (itemID = crypto.randomUUID()){
     dueDate.classList.add("dueDate");
     priority.classList.add("priority");
 
+    title.textContent = "Untitled";
+    dueDate.textContent = Date.now();
+    priority.textContent = 99;
+
     for (const element of [title, dueDate, priority]){
         div.appendChild(element);
     }
     return div;
 }
 
+const createProjectCard = function (itemID = crypto.randomUUID()){
+    let div = document.createElement("div");
+    div.dataset.itemID = itemID;
+
+    let name = document.createElement("h3");
+    let description = document.createElement("p");
+    let toDoItemCards = document.createElement("div");
+    
+    div.classList.add("projectCard");
+    name.classList.add("name");
+    description.classList.add("description");
+    toDoItemCards.classList.add("toDoItemCards");
+      
+    for (const element of [name, description, toDoItemCards]){
+        div.appendChild(element);
+    }
+    return div;
+}
+
 export {
-    createToDoItemCard
+    createToDoItemCard, createProjectCard
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import ToDoItem from "./toDoItem.js";
 import checkListManager from "./checkListManager.js"
 
+import {createToDoItemCard, createProjectCard} from "./cardFactories.js"
 import {updateToDoItemCard} from "./cardUpdaters.js"
-import {createToDoItemCard} from "./cardFactories.js"
+
 import Project from "./project.js";
 
 let toDoItem_1 = new ToDoItem();
 let toDoItem_2 = new ToDoItem("Eat more sushi", "Sushi is very expensive", undefined, 2);
-let toDoItem_3 = new ToDoItem(undefined, "Find the whereabouts of Abyssal Dision", undefined, 3);
+let toDoItem_3 = new ToDoItem("Goal in life", "Find the whereabouts of Abyssal Dision", undefined, 3);
 
 checkListManager.addCheckListItem(toDoItem_3, "Join UPEO");
 checkListManager.addCheckListItem(toDoItem_3, "Have human empathy")
@@ -22,6 +23,12 @@ let projectNemo = new Project("Nemo", "Default project", [
 
 console.log(projectNemo);
 
-const card = createToDoItemCard(toDoItem_1.id);
-updateToDoItemCard(toDoItem_3, card)
-document.querySelector("body").appendChild(card);
+const projectNemoCard = createProjectCard(projectNemo.id);
+for (const toDoItem of projectNemo.toDoItems){
+    const newCard =  createToDoItemCard(toDoItem.id);
+    updateToDoItemCard(toDoItem, newCard);
+
+    projectNemoCard.append(newCard);
+}
+
+document.querySelector("body").appendChild(projectNemoCard);
