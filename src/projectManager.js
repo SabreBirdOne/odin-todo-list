592318ea3575622b2f999f4742466763b08e6862
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-02-24 17:41:58 -0500

    Fix removeToDoFromProject in projectManager

--- src/projectManager.js
+++ src/projectManager.js
@@ -1,7 +1,7 @@
 const projectManager = (function (){
     const removeToDoFromProject = function(project, toDoItem){
-        const indexToRemove = project.toDoItems.findIndex((element) => element === toDoItem);
-        if (indexToRemove){
+        const indexToRemove = project.toDoItems.findIndex((element) => element.id === toDoItem.id.toString());
+        if (indexToRemove > -1){
             project.toDoItems.splice(indexToRemove, 1);
         }
         console.log(project.toDoItems);
