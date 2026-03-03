07403924a89970cbc9e3e133b5f2314b53e4beeb
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-03-03 16:51:16 -0500

    Style all dialogs on webpage
    Minor changes to home page styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./cards.css";
 import "./buttons.css";
 import "./checklist.css";
 import "./allProjectsDiv.css";
-import "./home.css"
+import "./home.css";
+import "./dialogs.css"
 
 loadHomePage();
